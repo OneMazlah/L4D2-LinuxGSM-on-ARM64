cec230146a38cde32b51bebe4a43c68cac373aa9
OneMazlah <afeqien777@gmail.com>
2026-03-16 14:39:21 +0800

    Update ARM wrapper library paths for LinuxGSM

--- srcds-arm.sh
+++ srcds-arm.sh
@@ -7,7 +7,16 @@ wrapper_log="${HOME}/log/console/srcds-arm-wrapper.log"
 cd "${serverfiles}"
 
 # Mirror the runtime environment that srcds_run normally prepares.
-export LD_LIBRARY_PATH=".:bin:${LD_LIBRARY_PATH:-}"
+armhf_lib_paths="/usr/lib/arm-linux-gnueabihf:/lib/arm-linux-gnueabihf"
+box86_x86_lib_paths="/usr/lib/box86-i386-linux-gnu"
+export LD_LIBRARY_PATH=".:bin:${armhf_lib_paths}:${LD_LIBRARY_PATH:-}"
+# Make both x86 runtime libraries bundled with box86 and armhf wrapped libraries
+# visible for x86 plugin loads such as MetaMod and SourceMod.
+export BOX86_LD_LIBRARY_PATH=".:bin:${box86_x86_lib_paths}:${armhf_lib_paths}:${BOX86_LD_LIBRARY_PATH:-}"
+compat_preload="${serverfiles}/bin/libisoc23compat.so"
+if [ -f "${compat_preload}" ]; then
+	export BOX86_LD_PRELOAD="${compat_preload}:${BOX86_LD_PRELOAD:-}"
+fi
 export BOX86_DYNAREC=1
 export BOX86_DYNAREC_BIGBLOCK=0
 export BOX86_DYNAREC_SAFEFLAGS=2
@@ -23,7 +32,7 @@ run_binary() {
 	local binary="$2"
 	shift 2
 
-	printf '[%s] starting %s %s (BOX86_DYNAREC=%s BIGBLOCK=%s SAFEFLAGS=%s)\n' "$(date -u '+%Y-%m-%dT%H:%M:%SZ')" "${runner}" "${binary}" "${BOX86_DYNAREC:-unset}" "${BOX86_DYNAREC_BIGBLOCK:-unset}" "${BOX86_DYNAREC_SAFEFLAGS:-unset}" >> "${wrapper_log}"
+	printf '[%s] starting %s %s (BOX86_DYNAREC=%s BIGBLOCK=%s SAFEFLAGS=%s PRELOAD=%s)\n' "$(date -u '+%Y-%m-%dT%H:%M:%SZ')" "${runner}" "${binary}" "${BOX86_DYNAREC:-unset}" "${BOX86_DYNAREC_BIGBLOCK:-unset}" "${BOX86_DYNAREC_SAFEFLAGS:-unset}" "${BOX86_LD_PRELOAD:-unset}" >> "${wrapper_log}"
 	set +e
 	"${runner}" "${binary}" "$@"
 	local status=$?
